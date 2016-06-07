d1d00148a7008f1d6e4c9f73bfb85649bbb1e9d5
benjamski <benjadamski@gmail.com>
2016-06-07 17:46:54 -0400

    added display user solutions to only display users symptoms table
    added placeholders in HB show symptoms

--- assets/scripts/display.js
+++ assets/scripts/display.js
@@ -10,10 +10,20 @@ let displaySolutions = function(solutions){
     }));
 };
 
+let displayUserSolutions = function(solutions){
+  console.log(solutions);
+    // $('.landing-div').hide(); //this hides the landing page div
+    // $('.show-solutions').html(''); //this clears the content in my table html
+  let solutionsListingTemplate = require('./templates/show-solutions.handlebars');
+    $('.show-solutions').append(solutionsListingTemplate({
+      symptom : solutions.symptom
+    }));
+};
 
 
 
 
 module.exports = {
-  displaySolutions
+  displaySolutions,
+  displayUserSolutions
 };

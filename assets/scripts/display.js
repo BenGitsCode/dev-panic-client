dc6b928a14135d514af3060e1ec758560cb99881
benjamski <benjadamski@gmail.com>
2016-06-07 19:06:40 -0400

    deleted console log
    removed log in displaySolutions

--- assets/scripts/display.js
+++ assets/scripts/display.js
@@ -1,7 +1,6 @@
 'use strict';
 
 let displaySolutions = function(solutions){
-  console.log(solutions);
     // $('.landing-div').hide(); //this hides the landing page div
     // $('.show-solutions').html(''); //this clears the content in my table html
   let solutionsListingTemplate = require('./templates/show-solutions.handlebars');

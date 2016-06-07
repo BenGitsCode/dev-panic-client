e4fd4e5ade167b1a03645ca5e71a761a159eb5c9
benjamski <benjadamski@gmail.com>
2016-06-07 18:40:20 -0400

    I think I know how to get HB rendering only solution
    testing now

--- assets/scripts/display.js
+++ assets/scripts/display.js
@@ -10,20 +10,20 @@ let displaySolutions = function(solutions){
     }));
 };
 
-let displayUserSolutions = function(solutions){
-  console.log(solutions);
-    // $('.landing-div').hide(); //this hides the landing page div
-    // $('.show-solutions').html(''); //this clears the content in my table html
-  let solutionsListingTemplate = require('./templates/show-solutions.handlebars');
-    $('.show-solutions').append(solutionsListingTemplate({
-      symptom : solutions.symptom
-    }));
-};
+// let displayUserSymptoms = function(solutions){
+//   console.log(solutions);
+//     // $('.landing-div').hide(); //this hides the landing page div
+//     // $('.show-solutions').html(''); //this clears the content in my table html
+//   let solutionsListingTemplate = require('./templates/show-user-symptom.handlebars');
+//     $('.show-user-symptom').append(solutionsListingTemplate({
+//       symptom : solutions.symptom
+//     }));
+// };
 
 
 
 
 module.exports = {
   displaySolutions,
-  displayUserSolutions
+  // displayUserSymptoms
 };
